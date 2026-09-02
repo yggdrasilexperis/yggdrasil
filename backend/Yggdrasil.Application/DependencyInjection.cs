356bd1f559ee8c16fd365cfe68673e3483f13a98
Fredrik Andreas Wiik <fredrik.wiik@no.experis.com>
2026-09-02 13:59:14 +0200

    feat: registered AuthService in DI chain

--- backend/Yggdrasil.Application/DependencyInjection.cs
+++ backend/Yggdrasil.Application/DependencyInjection.cs
@@ -2,6 +2,9 @@ using FluentValidation;
 
 using Microsoft.Extensions.DependencyInjection;
 
+using Yggdrasil.Application.Abstractions;
+using Yggdrasil.Application.Services;
+
 namespace Yggdrasil.Application;
 
 public static class DependencyInjection
@@ -9,6 +12,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+        services.AddScoped<IAuthService, AuthService>();
 
         return services;
     }
