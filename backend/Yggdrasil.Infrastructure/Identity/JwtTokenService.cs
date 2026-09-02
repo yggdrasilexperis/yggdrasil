baecf5a51801a44409b1f4b96dc9406959e2c2f7
Fredrik Andreas Wiik <fredrik.wiik@no.experis.com>
2026-09-02 14:42:31 +0200

    feat: implemented JwtTokenService to handle user-token logic

---
+++ backend/Yggdrasil.Infrastructure/Identity/JwtTokenService.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using System.Text;
+
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+using Yggdrasil.Application.Abstractions;
+using Yggdrasil.Application.Contracts.Authentication;
+using Yggdrasil.Application.Options;
+
+namespace Yggdrasil.Infrastructure.Identity;
+
+public class JwtTokenService(IOptions<JwtOptions> options) : ITokenService
+{
+    private readonly JwtOptions _jwtOptions = options.Value;
+    public AccessToken CreateAccessToken(UserResponse userAccount)
+    {
+        var expiresAt = DateTime.UtcNow.AddMinutes(_jwtOptions.AccessTokenExpirationInMinutes);
+        var descriptor = new SecurityTokenDescriptor
+        {
+            Issuer = _jwtOptions.Issuer,
+            Audience = _jwtOptions.Audience,
+            Expires = expiresAt,
+            Subject = new ClaimsIdentity(
+            [
+                new Claim(JwtRegisteredClaimNames.Sub, userAccount.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, userAccount.Email),
+                new Claim(JwtRegisteredClaimNames.Name, userAccount.UserName)
+            ]),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.IssuerSigningKey)),
+                SecurityAlgorithms.HmacSha256)
+        };
+
+        var token = new JsonWebTokenHandler().CreateToken(descriptor);
+
+        return new AccessToken(token, expiresAt);
+    }
+}
